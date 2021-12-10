0320ceda742772198bf26b9c34f2095dd15c9a76
Guilherme Viana <gaviana@rd.com.br>
2021-12-09 22:17:05 -0300

    Introducao Java

---
+++ src/br/com/rd/queroserdev/exercicio/Exercicio.java
@@ -0,0 +1,19 @@
+package br.com.rd.queroserdev.exercicio;
+
+public class Exercicio {
+	public static void main(String[] args) {
+		String produto = "Computador";
+		String produto2 = "Mesa";
+		
+		int idade = 30;
+		int codigo = 5290;
+		char genero = 'F';
+		
+		double preco = 2100.0;
+		double preco2 = 650.50;
+		
+		System.out.println("Produtos:\n" +produto + ", com preco de " + preco + " \n" + produto2 + ", com preco de "+ preco2);
+		System.out.println("Dados: " + idade + " anos, " + "codigo " + codigo);
+	}
+
+}
---
+++ src/br/com/rd/queroserdev/introducao/ExemploIntroducao.java
@@ -0,0 +1,31 @@
+package br.com.rd.queroserdev.introducao;
+
+public class ExemploIntroducao {
+	public static void main(String[] args) {
+		/*System.out.println("Hello Word!!!"); // coloca um \n ao final da impressao
+		System.out.println("Primeiro Programa!!!");
+
+		int x = 32;
+
+		System.out.println(x);
+
+		double y = 10.35784;
+
+		System.out.println("valor ponto flutuante");
+
+		System.out.println(y);
+		System.out.print(y);
+		System.out.printf("%.2f%n",y);
+		System.out.println("Exibição do npumero de ponto " + y +" todas as casas decimais");
+		*/
+		
+		String nome = "José";
+		int idade = 30;
+		double renda = 1000.0;
+		
+		System.out.printf("%s tem %d anos e uma renda de %.2f reais%n", nome, idade, renda);
+
+
+	}
+
+}
